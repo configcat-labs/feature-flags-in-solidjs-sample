dced2df1325e01ecab4dfc76f3b4e66eac8d7868
Chavez Harris <codedbychavez@gmail>
2022-09-02 15:06:07 -0300

    Configured feature flag into the app

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import { createSignal, Show } from "solid-js";
 import RandomColor from "./assets/RandomColor";
 import Palette from "./components/Palette";
 import FeatureNotAvailable from "./components/FeatureNotAvailable";
+import * as configcat from "configcat-js";
 
 function App() {
   const hexColors = ["#34568B", "#FF6F61", "#6B5B95", "#88B04B"];
-
-  const [canShowRandomColorGenerator, setRandomColorGenerator] =
+  const [canShowRandomColorGenerator, setCanShowRandomColorGenerator] =
     createSignal(true);
+
+  let configCatClient = configcat.createClient("6YzaCJW7q0WIJf6W33dEdg/UQcozJmaIkKYF55SxChDFw")
+
+  configCatClient.getValueAsync("canshowrandomcolorgenerator",  false)
+.then( value => {
+  setCanShowRandomColorGenerator(value);
+});
 
   return (
     <div className="app">
